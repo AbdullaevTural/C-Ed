66ff0151f6183875607189c248a0827725fbb6a2
TuralAbdullaev <tural.abdullaev@mail.ru>
2022-08-21 13:40:25 +0300

    общее

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("222Hello, World!");
+﻿
+
+Console.Write("n = ");
+int a = int.Parse(Console.ReadLine());
+
+if (n / 1000 > 0) Console.WriteLine("Invalid number");
+else {
+    int sum = 0, prod = 1;
+    while (n > 0) {
+        int rem;
+        n = Math.DivRem(n, 10, out rem);
+        sum += rem;
+        prod *= rem;
+    }
+    Console.WriteLine("Sum = {0}", sum);
+    Console.WriteLine("Product = {0}", prod);
+}
