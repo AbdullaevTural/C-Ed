a6b6930866d9399b4abb075fff45a46a88aa28ae
TuralAbdullaev <tural.abdullaev@mail.ru>
2022-08-21 15:59:02 +0300

    Практич. задание к уроку 1

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,17 +1,63 @@
-﻿
+﻿//Задача 2
 
-Console.Write("n = ");
-int a = int.Parse(Console.ReadLine());
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
 
-if (n / 1000 > 0) Console.WriteLine("Invalid number");
-else {
-    int sum = 0, prod = 1;
-    while (n > 0) {
-        int rem;
-        n = Math.DivRem(n, 10, out rem);
-        sum += rem;
-        prod *= rem;
-    }
-    Console.WriteLine("Sum = {0}", sum);
-    Console.WriteLine("Product = {0}", prod);
-}
+// int max = 0;
+
+// if (a < b) 
+// {
+//     max = b;
+// }
+// else 
+// {
+//      max = a;
+// }
+
+// Console.Write ("max=");
+// Console.WriteLine (max);
+
+//Задача 4
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// int c = Convert.ToInt32(Console.ReadLine());
+
+
+// int max = 0;
+
+// if ((a > b) && (a > c)) 
+// {
+//     max = a;
+// }
+// else if ((b > c) && (b > a) ) 
+// {
+//      max = b;
+// }
+// else 
+// {
+//      max = c;
+// }
+
+// Console.Write ("max=");
+// Console.WriteLine (max);
+
+//Задача 6
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// if(a%2 == 0){
+// Console.Write ("Число " + a  + " является четным" );
+// }
+// else {
+// Console.Write ("Число " + a  + " не является четным" );
+// }
+
+
+//Задача 8
+
+// int N = Convert.ToInt32(Console.ReadLine());
+// for(int i = 1; i <= N; i++)
+//     if(i%2 == 0){
+//     Console.WriteLine(i);
+//     }
+
